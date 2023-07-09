457dc70073488202570db94cd643bf35cef78693
draguar <33092622+draguar@users.noreply.github.com>
2023-07-09 17:03:49 +0200

    deezer_app

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+deezer_app = Flask(__name__)
 
-@app.route('/')
+@deezer_app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
+@deezer_app.route('/about')
 def about():
     return 'About'
