2fdb39670705d9561d56dac0258a8f4b8d0511a5
draguar <guillaume.vanel@protonmail.com>
2023-07-09 22:07:00 +0200

    tets

--- api/index.py
+++ api/index.py
@@ -10,15 +10,8 @@ from slack_sdk import WebClient
 import logging
 import sys
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-
-logger.addHandler(handler)
 
 # Use the logger to log messages
 logger.info('This is an info message')
