d7816f7d7bc806e60216a2b6991a49f07e7ff872
draguar <33092622+draguar@users.noreply.github.com>
2023-07-09 16:59:28 +0200

    minus imports

--- api/index.py
+++ api/index.py
@@ -2,20 +2,13 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, request, jsonify
-import os
-import requests
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-from slack_bolt.adapter.flask import SlackRequestHandler
-from slack_sdk import WebClient
-import logging
+
 
 
 deezer_app = Flask(__name__)
 
 @deezer_app.route('/')
 def hello_world():
-    logging.info("Request received at /")
     return "hey"
 
 
