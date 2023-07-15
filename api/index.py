91744b777c1086c1c1f1fce0b00f57fc47f7b4eb
draguar <guillaume.vanel@protonmail.com>
2023-07-15 18:47:54 +0200

    default slack bolt app_home_opened

--- api/index.py
+++ api/index.py
@@ -68,11 +68,60 @@ def hello():
     return "hello"
 
 
-@slack_app.event("app_home_opened")
-def handle_app_home_opened(event, client, logger):
-    user_id = event["user"]
-    update_home_view (user_id, event)
+# @slack_app.event("app_home_opened")
+# def handle_app_home_opened(event, client, logger):
+    # user_id = event["user"]
+    # update_home_view (user_id, event)
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
 
+        # body of the view
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+    app.logger.info("home tab updated")
+
+  except Exception as e:
+    app.logger.error(f"Error publishing home tab: {e}")
 
 
 
