f5fd81fd734fcf644dcf3b80e22b5df579effb40
draguar <guillaume.vanel@protonmail.com>
2023-07-27 21:13:26 +0200

    try to add cronjob

--- api/index.py
+++ api/index.py
@@ -18,7 +18,9 @@ DEEZER_CLIENT_SECRET = os.environ.get("DEEZER_CLIENT_SECRET")
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_USER_TOKEN = os.environ.get("SLACK_USER_TOKEN")
 PROJECT_URI  = os.environ.get("PROJECT_URI")
+CRONJOB_API_KEY = os.environ.get("CRONJOB_API_KEY")
 DEEZER_API_BASE_URL = "https://api.deezer.com/user/me/history"
+CRONJOB_API_BASE_URL = 'https://api.cron-job.org'
 
 deezer_access_tokens = {}
 uuid_to_slackID={}
@@ -48,7 +50,7 @@ def fetch_current_track():
                 # if track was started more than 5 minutes ago, we reset default status, otherwise we update it
                 if seconds_ago > 5*60:
                     update_slack_status("","",slack_id)
-                    return_value += "user " + slack_id + "isn't listening to deezer\n"
+                    return_value += "user " + slack_id + " isn't listening to deezer since : " + seconds_ago +"s \n"
                 else:
                     status_text = f"listening to: {current_track['title']} - {current_track['artist']['name']}"
                     update_slack_status(":musical_note:" , status_text, slack_id)
@@ -80,9 +82,12 @@ def update_slack_status(emoji, status_text, slack_id):
 @app.route('/')
 def hello_world():
     app.logger.info("Request received at /")
-    for slack_id, deezer_token in deezer_access_tokens.items():
-        return deezer_token
-    return PROJECT_URI
+    headers = {
+        'Authorization': 'Bearer '+ CRONJOB_API_KEY
+    }
+
+    result = requests.patch(CRONJOB_API_BASE_URL + '/jobs', headers=headers)
+    return result.json()
 
 @app.route("/deezyRedirect")
 def callback():
