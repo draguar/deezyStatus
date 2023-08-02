e78f041603a1c2f7d9089458c39aa8efadbe8059
draguar <guillaume.vanel@protonmail.com>
2023-08-02 15:59:09 +0200

    Update contentScript.js

--- DeezyTracker/contentScript.js
+++ DeezyTracker/contentScript.js
@@ -1,4 +1,7 @@
 const SLACK_APP_URL = "https://deezy-status.vercel.app"; 
+// Store the last sent emoji and status_text
+let lastEmoji = '';
+let lastStatusText = '';
 
 function isFavorite(){
 	const trackActions = document.querySelector(".track-actions");
@@ -20,6 +23,13 @@ function isFavorite(){
 
 function sendSlackStatus(emoji, status_text) {
 	console.log(emoji+ " "+ status_text);
+	
+	// Check if the current emoji and status_text are the same as the last sent ones
+    if (emoji === lastEmoji && status_text === lastStatusText) {
+        console.log('Data is the same as the previous request. Skipping Slack API call.');
+        return;
+    }
+
 	chrome.storage.sync.get('slackToken', function (data) {
 		// Create a data object with the parameters to send
 		const slack_post_data = {
@@ -36,6 +46,9 @@ function sendSlackStatus(emoji, status_text) {
 		  body: JSON.stringify(slack_post_data),
 	    };
 		
+		// Update the last sent emoji and status_text
+		lastEmoji = emoji;
+		lastStatusText = status_text;
 		// Send the POST request using fetch()
 		console.log("posting" + JSON.stringify(slack_post_data) + " to "+ `${SLACK_APP_URL}/slackstatus`);
 	    fetch(`${SLACK_APP_URL}/slackstatus`, requestOptions)
@@ -45,6 +58,7 @@ function sendSlackStatus(emoji, status_text) {
 		    }
 		    // Handle the successful response if needed
 		    console.log('Status update sent successfully');
+
 		  })
 		  .catch((error) => {
 		    // Handle any errors that occurred during the fetch
@@ -72,9 +86,11 @@ function logCurrentTrack() {
         console.log('Status: Paused');
 		sendSlackStatus("", "");
       } else if (pauseButton) {
-		const emoji = isFavorite() ? ":heart:" : ":musical_note:"; 
+		const favorite_status = isFavorite();
+		const emoji = favorite_status ? ":heart:" : ":musical_note:"; 
+		const status_text = "listening to" + (favorite_status?" favorite: ":": ") + track + " - " + artist
         console.log('Status: Playing, favorite: '+isFavorite());
-		sendSlackStatus(emoji, "listening to: "+ track + " - " + artist);
+		sendSlackStatus(emoji, status_text);
       } else {
         console.log('Status: Unknown');
       }
